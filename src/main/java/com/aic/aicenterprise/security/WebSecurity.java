328e00980c1c57c1d43623bc81a329ea9858abd2
Priyatam Vinnakota <vinnakota.priyatam@walmartlabs.com>
2020-11-09 01:44:49 +0530

    added admin paths to allow

--- src/main/java/com/aic/aicenterprise/security/WebSecurity.java
+++ src/main/java/com/aic/aicenterprise/security/WebSecurity.java
@@ -1,6 +1,5 @@
 package com.aic.aicenterprise.security;
 
-import com.aic.aicenterprise.services.UserService;
 import com.aic.aicenterprise.services.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -27,63 +26,63 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        String[] allowedPathsGET = {
-                "/auth/**",
-                "/static/**",
-                "/images/**",
-                "/favicon.ico",
-                "/api/cart/**",
-                "/api/orders/**",
-                "/api/products/**",
-                "/api/featured-products/**",
-                "/api/subscribers/**",
-                "/api/users/**",
-                "/api/brands/**",
-                "/",
-                "/login",
-                "/product-list",
-                "/products",
-                "/contact-us",
-                "/about-us",
-                "/covid19",
-                "/reset-password",
-                "/admin/**",
-                "/account/**",
-                "/confirm-email",
-                "/actuator/**"
-        };
-
-        String[] allowedPathsPOST = {
-                "/login",
-                "/api/products/load-products",
-                "/api/products/save", // TODO: remove later
-                "/api/products/delete", // TODO: remove later
-                "/api/featured-products/save", // TODO: remove later
-                "/api/featured-products/delete", // TODO: remove later
-                "/api/featured-products/load-products", // TODO: remove later
-                "/api/featured-products/delete-products", // TODO: remove later
-                "/api/orders/place-order",
-                "/api/orders/update-status", // TODO: remove later
-                "/api/users/sign-up",
-                "/api/users/save",
-                "/api/users/save-google-user",
-                "/api/users/update",
-                "/api/users/forgot-password",
-                "/api/users/reset-password",
-                "/api/users/user-image",
-                "/api/users/confirm-email",
-                "/api/users/send-confirm-email",
-                "/api/users/update-role", // TODO: remove later
-                "/api/subscribers/save",
-                "/api/cart/save",
-                "/api/brands/save", // TODO: remove later
-                "/api/brands/delete" // TODO: remove later
-        };
+//        String[] allowedPathsGET = {
+//                "/auth/**",
+//                "/static/**",
+//                "/images/**",
+//                "/favicon.ico",
+//                "/api/cart/**",
+//                "/api/orders/**",
+//                "/api/products/**",
+//                "/api/featured-products/**",
+//                "/api/subscribers/**",
+//                "/api/users/**",
+//                "/api/brands/**",
+//                "/",
+//                "/login",
+//                "/product-list",
+//                "/products",
+//                "/contact-us",
+//                "/about-us",
+//                "/covid19",
+//                "/reset-password",
+//                "/admin/**",
+//                "/account/**",
+//                "/confirm-email",
+//                "/actuator/**"
+//        };
+//
+//        String[] allowedPathsPOST = {
+//                "/login",
+//                "/api/products/load-products",
+//                "/api/products/save", // TODO: remove later
+//                "/api/products/delete", // TODO: remove later
+//                "/api/featured-products/save", // TODO: remove later
+//                "/api/featured-products/delete", // TODO: remove later
+//                "/api/featured-products/load-products", // TODO: remove later
+//                "/api/featured-products/delete-products", // TODO: remove later
+//                "/api/orders/place-order",
+//                "/api/orders/update-status", // TODO: remove later
+//                "/api/users/sign-up",
+//                "/api/users/save",
+//                "/api/users/save-google-user",
+//                "/api/users/update",
+//                "/api/users/forgot-password",
+//                "/api/users/reset-password",
+//                "/api/users/user-image",
+//                "/api/users/confirm-email",
+//                "/api/users/send-confirm-email",
+//                "/api/users/update-role", // TODO: remove later
+//                "/api/subscribers/save",
+//                "/api/cart/save",
+//                "/api/brands/save", // TODO: remove later
+//                "/api/brands/delete" // TODO: remove later
+//        };
 
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers(HttpMethod.GET, allowedPathsGET).permitAll()
-                .antMatchers(HttpMethod.POST, allowedPathsPOST).permitAll()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/**").permitAll()
                 .antMatchers(HttpMethod.PUT, "/**").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/**").permitAll()
                 .anyRequest().authenticated()
